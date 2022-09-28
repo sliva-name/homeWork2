33866cf7c2008752eb8de98211e6389a5f87cd28
sliva-name <antyuhov2@gmail.com>
2022-09-28 16:52:21 +0300

    prod

---
+++ userList/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using userList.Core;
+
+namespace userList
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool IsLogged { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in UserParser.Parse("users3.txt"))
+            {
+                if (login.Text == item.Login && password.Password == item.Password)
+                {
+                    IsLogged = true;
+
+                    if (item.Role == "Admin")
+                    {
+                        Admin admin = new Admin();
+                        admin.Show();
+                        MessageBox.Show($"Добро пожаловать {item.Name}");
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Добро пожаловать {item.Name}");
+                    }
+                                          
+                }
+                else
+                {
+                    IsLogged = false;
+                }
+            }
+            if (!IsLogged)
+            {
+                MessageBox.Show("Bad login and password");
+            }
+        }
+    }
+}
---
+++ userList/Models/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace userList.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime DateTime { get; set; }
+        public double Salary { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Login { get; set; }
+        public string Role { get; set; }
+        public User(int id, string middleName, string name, string lastName, DateTime dateTime, double salary, string login, string password, string role)
+        {
+            Id = id;
+            MiddleName = middleName;
+            Name = name;
+            LastName = lastName;
+            DateTime = dateTime;
+            Salary = salary;
+            Login = login;
+            Password = password;
+            Role = role;
+        }
+        public User()
+        {
+            Id = 0;
+            MiddleName = string.Empty;
+            Name = string.Empty;
+            LastName = string.Empty;
+            DateTime = DateTime.Date;
+            Salary = 0;
+            Login = string.Empty;
+            Password = string.Empty;
+            Role = string.Empty;
+        }
+
+        private string ConvertHashPassword(string password)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
